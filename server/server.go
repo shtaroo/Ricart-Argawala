0f41bb1f33bba9468a3f9ebc9a5152ce57aca4b5
Casper <Casper.meier.jenkins@gmail.com>
2025-11-06 12:59:35 +0100

    ran proto command

--- server/server.go
+++ server/server.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	proto "ITUServer/grpc"
-	"context"
 )
 
-type ITU_databaseServer struct {
-	proto.UnimplementedITUDatabaseServer
+type NodeGeneralServer struct {
+	proto.UnimplementedNodeServer
 	students []string
 }
 
-func (s *ITU_databaseServer) GetStudents(ctx context.Context, in *proto.Empty) (*proto.Students, error) {
-	return &proto.Students{Students: s.students}, nil
-}
+func
 
 func main() {
 
