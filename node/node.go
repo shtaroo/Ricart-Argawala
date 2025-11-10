ceb28c673fd1b54a4f5dd5208b899f43659b1a1b
oshe <oshe@itu.dk>
2025-11-10 18:43:18 +0100

    Starting node-server
    also modified some comments for clarity
    
    Co-Authored-By: Casper <225747490+shtaroo@users.noreply.github.com>

--- node/node.go
+++ node/node.go
@@ -1,46 +1,74 @@
 package main
 
 import (
-	"os"
-	"strconv"
+	log "log"
+	net "net"
+	os "os"
+	strconv "strconv"
+	time "time"
+
+	grpc "google.golang.org/grpc"
 
 	pb "module/proto"
 )
 
 // Node constructor
 type Node struct {
-	pb.UnimplementedNodeServer
+	pb.UnimplementedRicartArgawalaServer
 
-	node_id int
+	id      int
 	state   string
 	lamport int
 	peers   []string
 }
 
+func (node *Node) StartServer(port string) {
+	// Create listener
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	// Create and register server
+	server := grpc.NewServer()
+	pb.RegisterRicartArgawalaServer(server, node)
+
+	// Log for transparency
+	log.Printf("Node #%d now listening on localhost%s", node.id, port)
+
+	// Serve
+	if err := server.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
+
 func main() {
 	if len(os.Args) < 4 {
-		// Even though we're really looking for 3 arguments, we check for less than 4.
-		// This is because the first (0th) argument always directs to the .exe of the .go file that's run
-		println("Usage (from root dir): go run node/node.go <node_id> :<port_num> <peer_1> [<peer_2> ...]")
+		// Even though we're really looking for 3 arguments, we expect 4 or more because
+		// the first (0th) argument always directs to the .exe of the .go file that's run...
+		println("Usage: go run node.go <node_id> :<port_num> <peer_1> [<peer_2> ...]")
 		return
 	}
 
-	// Parse args to create a new Node
+	// Parse args to create a new Node and server
 	node_id, _ := strconv.Atoi(os.Args[1])
 	port := os.Args[2]
 	peers := os.Args[3:]
 
 	// Create new Node
 	node := &Node{
-		node_id: node_id,
+		id:      node_id,
 		state:   "RELEASED",
 		lamport: 0,
 		peers:   peers,
 	}
 
-	// temp / debug
-	println(os.Args[0])
-	println(port)
-	println(node)
-	println(peers)
+	// Start node-server (start listening for requests)
+	node.StartServer(port)
+
+	// Wait 20 seconds so there is time to start all Nodes
+	time.Sleep(20 * time.Second)
+
+	// Continuously attempt to enter critical section
+	// code goes here ...
 }
